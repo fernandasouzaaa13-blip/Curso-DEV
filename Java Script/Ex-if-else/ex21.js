ee548595461f5bd7150edb8105d88fb5b9edabc5
fernandasouzaaa13-blip <fernandasouzaaa13@gmail.com>
2026-05-29 22:45:22 -0300

    Atualizacao

---
+++ Java Script/Ex-if-else/ex21.js
@@ -0,0 +1,11 @@
+let sinal = "Verde"
+
+if (sinal === "Verde"){
+    console.log("Pode ir")
+}
+if (sinal === "Amarelo") {
+    console.log("Atenção")
+}
+if (sinal === "Vermelho") {
+    console.log("Pare")
+}
