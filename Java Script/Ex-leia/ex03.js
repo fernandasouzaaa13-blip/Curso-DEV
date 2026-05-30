0067d598418ed1cdfa2821fb98ffab8e2a58f86e
fernandasouzaaa13-blip <fernandasouzaaa13@gmail.com>
2026-05-29 23:53:51 -0300

    Atualizações

--- Java Script/Ex-leia/ex03.js
+++ Java Script/Ex-leia/ex03.js
@@ -2,12 +2,12 @@ const leia = require("prompt-sync")()
 let altura = leia("Digite sua altura: ")
 
 
-if (altura>=40){
-    console.log("Pode entrar")
+if (altura>=1.40){
+    console.log("Pode entrar sozinho")
 }
 if (altura >= 1.20 && altura <= 1.39) {
   console.log("Pode entrar acompanhado")
 }
-if (altura < 1,20) {
+if (altura < 1.20) {
   console.log("Não pode entrar")
   }
