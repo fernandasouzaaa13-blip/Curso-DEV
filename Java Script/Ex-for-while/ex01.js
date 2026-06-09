3263e181b7d569e4353302fb93d9b556d16ef1d2
Maria Fernanda <fernandasouzaaa13@gmail.com>
2026-06-08 21:48:24 -0300

    Exercicios de repetição

---
+++ Java Script/Ex-for-while/ex01.js
@@ -0,0 +1,4 @@
+for(let contador=1 contador <= 10,contador++){ 
+    console.log("Estou contando de: "+contador)
+contador++
+}
---
+++ Java Script/Ex-for-while/ex02.js
@@ -0,0 +1,6 @@
+let contador = 10
+while(contador>0){
+    console.log("Estou contando regressivo agora: ")
+    console.log(contador)
+    contador--
+}
