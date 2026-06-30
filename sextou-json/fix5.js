e98d52a34e035c50d97ea5309a524de0edbeadb2
Maria Fernanda <fernandasouzaaa13@gmail.com>
2026-06-30 19:35:39 -0300

    Atualizações

--- sextou-json/fix5.js
+++ sextou-json/fix5.js
@@ -1,21 +1,6 @@
-// Criando um array com os preços.
-const precos = [20, 50, 100, 80];
-
-// O método map() percorre todos os elementos do array
-// criando um novo array.
-//
-// Cada preço será multiplicado por 1.15,
-// que representa um aumento de 15%.
-const novosPrecos = precos.map(function(preco) {
-
-    // Calculando o novo valor.
-    return preco * 1.15;
-
-});
-
-// Exibindo o novo array.
-console.log(novosPrecos);
-
-// Resultado:
-// [23, 57.5, 115, 92]
+//Criando um array
+const precos = [20,50,100,80];
+const novosPrecos= precos.map(preco=>preco*1.15);
+console.log(precos);
+console.log(novosPrecos)
 
--- sextou-json/fix7.js
+++ sextou-json/fix7.js
@@ -1,12 +1,17 @@
-// Array de produtos
-const produtos = [
-    { nome: "Mochila", preco: 89 },
-    { nome: "Caneta", preco: 3 },
-    { nome: "Livro", preco: 45 }
+const produto = [
+  {
+    nome: "Mochila",
+    preco: 89,
+  },
+
+  {
+    nome: "Caneta",
+    preco: 3,
+  },
+
+  {
+    nome: "Livro",
+     preco: 45,
+  },
 ];
 
-// Encontrando o primeiro produto abaixo de 30 reais
-const produto = produtos.find(item => item.preco < 30);
-
-// Exibindo apenas o nome
-console.log(produto.nome);
