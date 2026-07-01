068c813d352a255959821a3d6d722463e935461f
fernandasouzaaa13-blip <fernandasouzaaa13@gmail.com>
2026-07-01 17:23:58 -0300

    Alterações

--- Java Script/JSON-Js/exf1.js
+++ Java Script/JSON-Js/exf1.js
@@ -1,71 +1,56 @@
-//Criando o objeto
-const produtos =[
-{
-    id:1,
-    nome:"Mouse",
-    preco: 80,
-    estoque: 10
-},
-
-{
-    id:2,
-    nome:"Teclado",
-    preco: 150,
-    estoque: 5
-},
-{
-    id:3,
-    nome:"Monitor",
-    preco: 1200,
-    estoque: 2
-}
+// Criando o array de produtos.
+const produtos = [
+    {
+        id: 1,
+        nome: "Mouse",
+        preco: 80,
+        estoque: 10
+    },
+    {
+        id: 2,
+        nome: "Teclado",
+        preco: 150,
+        estoque: 5
+    },
+    {
+        id: 3,
+        nome: "Monitor",
+        preco: 1200,
+        estoque: 2
+    }
 ];
 
-const novoProduto={
-    id:4,
-    nome:"Mesa",
-    preco:350,
-    estoque:7
+// Criando um novo produto que será adicionado ao estoque.
+const novoProduto = {
+    id: 4,
+    nome: "Mesa",
+    preco: 350,
+    estoque: 7
 };
-produto.push(novoProduto);
 
-console.log(novoProduto)
+// O método push() adiciona um novo elemento ao final do array.
+produtos.push(novoProduto);
 
-const indice = 
+// Exibindo o array após adicionar o novo produto.
+console.log(produtos);
 
+// O método findIndex() percorre o array procurando o primeiro produtom
+const indice = produtos.findIndex(produto => produto.id === 2);
+// Exibindo o índice encontrado.
+console.log(indice);
 
+// O método splice() remove elementos do array.
+//Será removido apenas o produto com id igual a 2.
+produtos.splice(indice, 1);
 
+// Exibindo o array após remover o produto.
+console.log(produtos);
 
+// O operador delete remove uma propriedade de um objeto.
+// O objeto continua existindo, apenas a propriedade é removida.
+delete produtos[0].preco;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Convertendo o objeto para String JSON
-const json = JSON.stringify(produto);
-//Exibindo o JSON
-console.log(json);
-//Verificando o tipo
-console.log(typeof json);
+// Exibindo o array após remover a propriedade.
+console.log(produtos);
+// Tipo 
+console.log(typeof produtos)
