af7f2bf473471a046b002fc830adadd8b0ba0f73
fernandasouzaaa13-blip <fernandasouzaaa13@gmail.com>
2026-08-06 16:14:44 -0300

    Alterações

--- Java_/Arraylist/Lista7.java
+++ Java_/Arraylist/Lista7.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Lista7 {
     public static void main(String[] args) {
         ArrayList<String> produtos = new ArrayList<>();
-        // Imprir a lista -> imprima todos os protudos utilizando forEach
+        System.out.println("========= PRODUTOS ==========");
+        // Exercício 01 -Imprir a lista -> imprima todos os protudos utilizando forEach
         produtos.add("Mouse");
         produtos.add("Teclado");
         produtos.add("Monitor");
@@ -14,41 +15,57 @@ public class Lista7 {
 
         produtos.forEach(System.out::println);
 
-        System.out.println("===================================");
-        // Remover um produto -> Remova o produto "Monitor" e imprima lista novamente
+        System.out.println("=============================");
+        // Exercício 02- Remover um produto -> Remova o produto "Monitor" e imprima
+        // lista novamente
         produtos.remove(2);
         produtos.forEach(System.out::println);
-        System.out.println("===================================");
-        // Contar produtos -> Ultilize size() para mostrar quantos produtos existem na
+          System.out.println("=============================");
+        // Exercício 03- Contar produtos -> Ultilize size() para mostrar quantos
+        // produtos existem na
         // lista
         produtos.forEach(System.out::println);
         {
             System.out.println("Total de produtos: " + produtos.size());
 
-            System.out.println("===================================");
+            System.out.println("=============================");
 
-            // Verificar se existe -> Verifique se o produto "Mouse" está na lista
+            // Exercício 04-Verificar se existe -> Verifique se o produto "Mouse" está na
+            // lista
             // utilizando contains().
             // Obs: Deve ser tratado com IF se foi ou não encontrado
-
+            produtos.forEach(System.out::println);
             boolean resp = produtos.contains("Mouse");
-            if (resp == true) {
+            if (resp) {
                 System.out.println("O Mouse está dentro da lista");
             } else {
                 System.out.println("O Mouse não está na lista");
             }
-            produtos.forEach(System.out::println);
-
-            System.out.println("===================================");
-            // Alterar um produto -> Troque "Headset" por " Caixa de Som" utilizando set().
-            // Depois imprima a lista
-
-            produtos.set(3, "Caixa de Som");
-            produtos.forEach(System.out::println);
-
-            // Mostrar um produto específico -> Utilize get()para mostrar o terceiro produto
-            // da lista. Saída esperada: Terceiro produto: Monitor
-
         }
+        /*
+         * ou if (produtos.contains("Mouse")) {
+         * System.out.println("O Mouse está dentro da lista");
+         * } else {
+         * System.out.println("O Mouse não está na lista");
+         */
+
+            System.out.println("=============================");
+        // Exercício 05- Alterar um produto -> Troque "Headset" por "Caixa de Som"
+        // utilizando set().
+        // Depois imprima a lista
+        // Altera o produto que está na posição 3 da lista para "Caixa de Som"
+        produtos.set(3, "Caixa de Som");
+        // Percorre a lista e imprime todos os produtos atualizados
+        produtos.forEach(System.out::println);
+           System.out.println("=============================");
+
+        // Exercício 06- Mostrar um produto específico -> Utilize get()para mostrar o
+        // terceiro produto
+        // da lista. Saída esperada: Terceiro produto: Monitor
+
+        // Exibe o produto que está na posição 2 da lista (terceiro produto, pois a
+        // contagem começa em 0)
+        System.out.println("Terceiro produto: " + produtos.get(3));
+
     }
 }
