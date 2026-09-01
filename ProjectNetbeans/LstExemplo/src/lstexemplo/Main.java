9ecd4b22bb47e893b7e9e9f3f3b0511f4a470120
Maria Fernanda <fernandasouzaaa13@gmail.com>
2026-09-01 20:16:03 -0300

    Alterações

---
+++ ProjectNetbeans/LstExemplo/src/lstexemplo/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lstexemplo;
+
+/**
+ *
+ * @author 1104695
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tela telaLayout = new Tela();
+        telaLayout.setVisible(true);
+        //new Tela().setVisible(true);
+    }
+
+}
